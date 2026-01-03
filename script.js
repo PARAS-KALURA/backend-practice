1804f195bfa653b2e71a24569af8e95575c86149
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-03 15:24:08 +0530

    1B

--- script.js
+++ script.js
@@ -6,11 +6,28 @@ const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static('./public'));
 
 app.get('/', function (req, res){
-  res.render("index");
+  res.render("index", {age: 12});
 })
 
+app.get('/error', function (req, res, next) {
+  next(new Error("Something went wrong"));
+});
+
+
+app.get('/contact', function (req, res){
+  res.render("contact", {name: "Paras Kalura"});
+} )
+
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
