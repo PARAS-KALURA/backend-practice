fb3c233baa2ae0713b5a155c91b891a413dfabbe
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-06 21:32:07 +0530

    DONE

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json()); // 👈 MUST be before routes
+app.use(express.urlencoded({ extended: true }));
+
 
 app.post("/user", (req, res) => {
   console.log(req.body);
